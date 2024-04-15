a3e62b0039bad17c8b4e8e56a19e46be39116e70
militsiya04 <denysmushkatyorov@gmail.com>
2024-04-15 22:38:14 +0300

    Done task 2-4 in lab4

---
+++ Lab2/Lab4/Data/AdoAssistant.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace Lab4.Data;
+
+public class AdoAssistant
+{
+    private string connectionString = ConfigurationManager.ConnectionStrings["connectionString_ADO"].ConnectionString;
+
+    public DataTable TableLoad()
+    {
+        DataTable dt = new DataTable();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT * FROM products";
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(dt);
+            connection.Close();
+        }
+
+        return dt;
+    }
+
+    // Метод для виконання SQL-запита, що не повертає результат
+    private void ExecuteNonQuery(string query)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            // Відкриваємо з'єднання
+            connection.Open();
+
+            // Створюємо команду SQL
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                try
+                {
+                    // Виконуємо команду
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Done");
+                }
+                catch (Exception ex)
+                {
+                    // Обробка помилки, наприклад, виведення її на консоль або логування
+                    MessageBox.Show($"Error : {ex.Message}");
+                }
+                finally
+                {
+                    // Закриваємо підключення у блоку finally, щоб гарантувати його виконання
+                    if (connection.State == ConnectionState.Open)
+                        connection.Close();
+                }
+            }
+        }
+    }
+
+    // Метод для додавання запису в базу даних
+    public void AddRecord(string article, string name, string unitOfMeasure, int quantity, float price)
+    {
+        string query =
+            $"INSERT INTO products (article, name, unit_of_measure, quantity, price) VALUES ('{article}', '{name}', '{unitOfMeasure}', {quantity}, {price})";
+        ExecuteNonQuery(query);
+    }
+
+    // Метод для оновлення запису в базі даних
+    public void UpdateRecord(string article, string name, string unitOfMeasure, int quantity, float price)
+    {
+        string query =
+            $"UPDATE products SET name = '{name}', unit_of_measure = '{unitOfMeasure}', quantity = {quantity}, price = {price.ToString().Replace(',','.')} WHERE article = '{article}'";
+        ExecuteNonQuery(query);
+    }
+
+    // Метод для видалення запису з бази даних
+    public void DeleteRecord(string article)
+    {
+        string query = $"DELETE FROM products WHERE article = '{article}'";
+        ExecuteNonQuery(query);
+    }
+}
--- Lab2/Lab4/MainWindow.xaml.cs
+++ Lab2/Lab4/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Lab4.Data;
 
 namespace Lab4
 {
@@ -23,6 +14,94 @@ namespace Lab4
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Load();
+        }
+
+        private void Load()
+        {
+            AdoAssistant myTable = new AdoAssistant();
+            list.Focus();
+            list.DataContext = myTable.TableLoad(); 
+            list.SelectedIndex = 0;
+
+        }
+
+        private void MainWindow_OnClosing(object? sender, CancelEventArgs e)
+        {
+            Close();
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedIndex != -1)
+            {
+                MessageBox.Show("Press 'Add' button");
+                return;
+            }
+
+            if (String.IsNullOrEmpty(ArticleTextBox.Text) || String.IsNullOrEmpty(NameTextBox.Text) || String.IsNullOrEmpty(UnitOfMeasureTextBox.Text) || String.IsNullOrEmpty(QuantityTextBox.Text) || String.IsNullOrEmpty(PriceTextBox.Text))
+            {
+                MessageBox.Show("Enter all rows");
+                return;
+            }
+            AdoAssistant myAssistant = new AdoAssistant();
+            if (Int32.TryParse(QuantityTextBox.Text, out int temp1) && float.TryParse(PriceTextBox.Text.Replace('.',','), out float temp2))
+            {
+                myAssistant.AddRecord(ArticleTextBox.Text,NameTextBox.Text,UnitOfMeasureTextBox.Text, Int32.Parse(QuantityTextBox.Text) ,float.Parse(PriceTextBox.Text.Replace('.',',')) );
+            }
+            else
+            {
+                MessageBox.Show("Enter the correct value");
+                return;
+            }
+            Load();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedIndex == -1)
+            {
+                MessageBox.Show("Оберіть продукт який необхідно змінити");
+                return;
+            }
+            
+            if (String.IsNullOrEmpty(ArticleTextBox.Text) || String.IsNullOrEmpty(NameTextBox.Text) || String.IsNullOrEmpty(UnitOfMeasureTextBox.Text) || String.IsNullOrEmpty(QuantityTextBox.Text) || String.IsNullOrEmpty(PriceTextBox.Text))
+            {
+                MessageBox.Show("Enter all rows");
+                return;
+            }
+            AdoAssistant myAssistant = new AdoAssistant();
+            if (Int32.TryParse(QuantityTextBox.Text, out int temp1) && float.TryParse(PriceTextBox.Text.Replace('.',','), out float temp2))
+            {
+                myAssistant.UpdateRecord(ArticleTextBox.Text,NameTextBox.Text,UnitOfMeasureTextBox.Text, Int32.Parse(QuantityTextBox.Text) ,float.Parse(PriceTextBox.Text.Replace('.',',')) );
+            }
+            else
+            {
+                MessageBox.Show("Enter the correct value");
+                return;
+            }
+            Load();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (list.SelectedIndex == -1)
+            {
+                MessageBox.Show("Оберіть продукт який необхідно видалити");
+                return;
+            }
+            AdoAssistant myAssistant = new AdoAssistant();
+            myAssistant.DeleteRecord(ArticleTextBox.Text);
+            Load();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            list.SelectedIndex = -1;
         }
     }
 }
